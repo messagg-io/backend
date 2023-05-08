829c4604514ab02005b7b6c131ec400a4e6737a8
kulichkoff <kulichkov@background.digital>
2023-05-08 18:58:04 +0300

    test(e2e): users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { CreateUserDto } from '@app/users/dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -14,5 +15,10 @@ export class UsersController {
   @Get(':username')
   public async findOne(@Param('username') username: string) {
     return this.usersService.findOne(username);
+  }
+
+  @Post()
+  public async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 }
